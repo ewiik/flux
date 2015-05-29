93a61034a6881c9708116d0af83c902f2bfe53aa
ewiik <eme.wiik@gmail.com>
2015-05-29 15:12:50 -0600

    hard-wrapped some lines

--- scripts/gasexchange_pressuredata.R
+++ scripts/gasexchange_pressuredata.R
@@ -134,10 +134,15 @@ getData <- function(stations, folder, verbose = TRUE, delete = TRUE) {
 
 ## my station data frame
 
-# nb all stations can cover my year range apart from outlook which has hourly only from 1996... there are randomly monthly data going way back but the monthly interface does not show pressure... if we want to request data it costs.... $100!!!! http://climate.weather.gc.ca/newPriceAnnounce_e.html
+# nb all stations can cover my year range apart from outlook which has hourly only from 1996... there 
+# are randomly monthly data going way back but the monthly interface does not show pressure... if we want to request 
+# data it costs.... $100!!!! http://climate.weather.gc.ca/newPriceAnnounce_e.html
 
 #maximum example now
-stations <- data.frame(StationID = c(3002, 51441, 3062, 44203, 48977, 3318, 2926, 2925), start = c(1994, 2013, 1994, 2005, 2011, 1996, 1994, 1996), end = c(2013, 2015, 2005, 2011, 2015, 2015, 1996, 2015)) # see weatherstations.csv in /RScripts; these will cover lakes b,c,d,k,l,p,ww.
+stations <- data.frame(StationID = c(3002, 51441, 3062, 44203, 48977, 3318, 2926, 2925), 
+		start = c(1994, 2013, 1994, 2005, 2011, 1996, 1994, 1996), 
+		end = c(2013, 2015, 2005, 2011, 2015, 2015, 1996, 2015)) 
+			# see weatherstations.csv in /RScripts; these will cover lakes b,c,d,k,l,p,ww.
 
 met <- getData(stations, folder = "./weatherdata")
 
