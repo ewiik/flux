88d21409b4348fe9f86cba20628dc417a32386c1
Gavin Simpson <ucfagls@gmail.com>
2015-06-02 17:38:25 -0600

    update the directory where we store the CSVs

--- scripts/gasexchange_pressuredata.R
+++ scripts/gasexchange_pressuredata.R
@@ -146,4 +146,4 @@ stations <- data.frame(StationID = c(3002, 51441, 3062, 44203, 48977,
                        end = c(2013, 2015, 2005, 2011, 2015, 2015, 1996, 2015))
 ## see weatherstations.csv in /RScripts; these will cover lakes b,c,d,k,l,p,ww.
 
-met <- getData(stations, folder = "./weatherdata")
+met <- getData(stations, folder = "../data/weatherdata")
