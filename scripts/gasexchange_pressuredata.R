422570368a3a7d5aa1a6a89f1db63f7b0101cabc
Gavin Simpson <ucfagls@gmail.com>
2015-06-02 10:02:15 -0600

    tweak the setting of names()

--- scripts/gasexchange_pressuredata.R
+++ scripts/gasexchange_pressuredata.R
@@ -37,7 +37,7 @@ getData <- function(stations, folder, verbose = TRUE, delete = TRUE) {
                                stations$start[i],
                                stations$end[i])
                    }, stations = stations)
- 
+
     ## check the folder exists and try to create it if not
     if (!file.exists(folder)) {
         warning(paste("Directory:", folder,
@@ -48,25 +48,25 @@ getData <- function(stations, folder, verbose = TRUE, delete = TRUE) {
                  "'. Check path and permissions.", sep = "")
         }
     }
- 
+
     ## Extract the data from the URLs generation
     URLS <- unlist(lapply(urls, '[[', "urls"))
     sites <- unlist(lapply(urls, '[[', "ids"))
     years <- unlist(lapply(urls, '[[', "years"))
     months <- unlist(lapply(urls, '[[', "months"))
- 
+
     ## filenames to use to save the data
     fnames <- paste(sites, years, months, "data.csv", sep = "-")
     fnames <- file.path(folder, fnames)
- 
+
     nfiles <- length(fnames)
- 
+
     ## set up a progress bar if being verbose
     if (isTRUE(verbose)) {
         pb <- txtProgressBar(min = 0, max = nfiles, style = 3)
         on.exit(close(pb))
     }
- 
+
     out <- vector(mode = "list", length = nfiles)
     cnames <- c("Date/Time", "Year", "Month","Day", "Time", "Data Quality",
                 "Temp (degC)", "Temp Flag", "Dew Point Temp (degC)",
@@ -75,10 +75,10 @@ getData <- function(stations, folder, verbose = TRUE, delete = TRUE) {
                 "Wind Spd Flag", "Visibility (km)", "Visibility Flag",
                 "Stn Press (kPa)", "Stn Press Flag", "Hmdx", "Hmdx Flag",
                 "Wind Chill", "Wind Chill Flag", "Weather")
- 
+
     for (i in seq_len(nfiles)) {
         curfile <- fnames[i]
- 
+
         ## Have we downloaded the file before?
         if (!file.exists(curfile)) {    # No: download it
             dload <- try(download.file(URLS[i], destfile = curfile, quiet = TRUE))
@@ -90,12 +90,12 @@ getData <- function(stations, folder, verbose = TRUE, delete = TRUE) {
                 next                             # bail out of current iteration
             }
         }
- 
+
         ## Must have downloaded, try to read file
         ## skip first 16 rows of header stuff
         ## encoding must be latin1 or will fail - may still be problems with character set
         cdata <- try(read.csv(curfile, skip = 16, encoding = "latin1"), silent = TRUE)
- 
+
         ## Did we have a problem reading the data?
         if (inherits(cdata, "try-error")) { # yes handle read problem
             ## try to fix the problem with dodgy characters
@@ -116,32 +116,32 @@ getData <- function(stations, folder, verbose = TRUE, delete = TRUE) {
                 next                  # bail out of current iteration
             }
         }
- 
+
         ## Must have (eventually) read file OK, add station data
         cdata <- cbind.data.frame(StationID = rep(sites[i], NROW(cdata)),
                                   cdata)
-        names(cdata) <- c("StationID", cnames) # ew edited here
+        names(cdata)[-1] <- cnames
         out[[i]] <- cdata
- 
+
         if (isTRUE(verbose)) { # Update the progress bar
             setTxtProgressBar(pb, value = i)
         }
     }
- 
+
     out                                 # return
     saveRDS(out, "outgas.rds") # ew edited here
 }
 
 ## my station data frame
 
-# nb all stations can cover my year range apart from outlook which has hourly only from 1996... there 
-# are randomly monthly data going way back but the monthly interface does not show pressure... if we want to request 
+# nb all stations can cover my year range apart from outlook which has hourly only from 1996... there
+# are randomly monthly data going way back but the monthly interface does not show pressure... if we want to request
 # data it costs.... $100!!!! http://climate.weather.gc.ca/newPriceAnnounce_e.html
 
 #maximum example now
-stations <- data.frame(StationID = c(3002, 51441, 3062, 44203, 48977, 3318, 2926, 2925), 
-		start = c(1994, 2013, 1994, 2005, 2011, 1996, 1994, 1996), 
-		end = c(2013, 2015, 2005, 2011, 2015, 2015, 1996, 2015)) 
+stations <- data.frame(StationID = c(3002, 51441, 3062, 44203, 48977, 3318, 2926, 2925),
+		start = c(1994, 2013, 1994, 2005, 2011, 1996, 1994, 1996),
+		end = c(2013, 2015, 2005, 2011, 2015, 2015, 1996, 2015))
 			# see weatherstations.csv in /RScripts; these will cover lakes b,c,d,k,l,p,ww.
 
 met <- getData(stations, folder = "./weatherdata")
