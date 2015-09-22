83255a7491a532b241b9ec3810179867f8b337e8
ewiik <eme.wiik@gmail.com>
2015-09-21 18:46:05 -0600

    started adding wind stuff to this script: readRDS, optional clauses for which superstations to use, added Day to joined so as to develop the wind averages, got to point where means need to be incorporated

--- scripts/pressuremanipulations.R
+++ scripts/pressuremanipulations.R
@@ -7,6 +7,12 @@ if (file.exists("data/pressuredata.rds")) {
   print("run weathermanipulations.R")
   }
 
+if (file.exists("data/windsdata.rds")) {
+  windsdata <- readRDS("data/windsdata.rds")  
+} else {
+  print("run weathermanipulations.R")
+}
+
 if (file.exists("data/private/fluxquery1.csv")) { # DIC, pH, wind
   surfd <- read.csv("data/private/fluxquery1.csv")
 } else {
@@ -61,21 +67,53 @@ joined <- merge(joined, subset(elevd, select = c("Abbreviation", "Elevation_m"))
                 by.x = "Lake", by.y = "Abbreviation", sort = FALSE)
 names(joined)[names(joined) == "Elevation_m"] <- "Elevation"
 
-## merge data on weather superstation for each lake
-joined <- merge(joined, superstations, sort = FALSE)
+## choice of using or not using all superstations
+regina <- TRUE
 
-## merge joined with pressure data
-## add Year & Month fields to aid the match
-##
-## FIXME: Still need to decide what to do with missing pressure data.
-##        Do we use pressure data from other near site? If so, why not use
-##        that for all the data not just missing stuff?
+if(regina) {
+  joined$Superstation <- rep("regina")
+} else {
+  joined <- merge(joined, superstations, sort = FALSE)
+  }
+
+## merge joined with pressure and wind data
+## add Year, Month and Day fields to aid the match
+## FIXME: Still need to decide if and what to do with missing pressure data.
+## FIXME: Need not match metWind with day of sampling but calculate previous 2 week average
 joined <- transform(joined, Year = as.numeric(format(Date, format = "%Y")),
-                    Month = as.numeric(format(Date, format = "%m")))
+                    Month = as.numeric(format(Date, format = "%m")),
+                    Day = as.numeric(format(Date, format = "%d")))
 joined <- merge(joined, pressuredata, sort = FALSE, all.x = TRUE)
+
+# avoid ambiguity and change colname for met wind; add Date column
+names(windsdata)[6] <- "metWind"
+
+toDate <- function(year, month, day) {
+  ISOdate(year, month, day)
+}
+
+windsdata$Date <- with(windsdata, toDate(Year, Month, Day))
+# windsdata <- transform(windsdata, Date = ) ... do we need to take away hours?
+
+
+# create appropriate averages for each instance (i.e. sampling date minus 14 days)
+## FIXME: this is where I left off. Continue!! 
+if(regina) {
+  windsub <- subset(windsdata, Superstation == "regina")
+  sampledates <- c(joined$Date)[1:14] # minimum example practice
+  interval <- list()
+  for (i in 1:length(sampledates)) {
+              interval[[i]] <- seq.Date(from = as.Date(sampledates[i]), by = -1, length.out = 14)
+  }
+  # grab date matches from windsdata for wind values,... an %in% ??
+  joined$metWind <- mean(winds) # something like this here but winds appropriate object
+}
+
+joined <- merge(joined, windsdata, sort = FALSE, all.x = TRUE)
 
 ## need to change wind from km/h to m/s
 joined <- transform(joined, Wind = Wind * (1000/60/60))
+joined <- transform(joined, metWind = metWind * (1000/60/60))
 
 ## need to change dic from mg/L to uM
 joined <- transform(joined, TIC = TIC / 0.012)
