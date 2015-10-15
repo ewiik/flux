fbb03c5b0f617b036fa902759b029bd2c6cf8b9b
ewiik <eme.wiik@gmail.com>
2015-10-15 17:49:58 -0600

    updated FIXMEs with new considerations and completed web page data download, making the file exists sections simpler. Looked into how many NAs and created some tables with this information too.

--- scripts/co2ExplVar.R
+++ scripts/co2ExplVar.R
@@ -1,66 +1,107 @@
-## This script creates an rds that contains all relevant data for running regressions against pCO2
+## This script creates rds's that contain all relevant data for running regressions against pCO2:
+## pdo.rds, soi_nonstand.rds, soi_stand.rds, naoseasonal.rds, co2explained.rds
 ## 1. download weather pattern data from web: PDO, SOI, NAO
 ## 2. process routines sampling data tables
 ## 3. get flow et al data from other sources
 
-## Part 1:
+## Part 1: all these data are complete
 ## ==================================================================================================
 
-## urls, and filenames to use to save the data
-
-dload <- readLines("http://research.jisao.washington.edu/pdo/PDO.latest", n = 148)
-dload2 <- dload[-c(1:30, 32)]
-dload2[117] <- paste(dload2[117], " NA NA NA NA")
-dtable <- dload2
-
-dtable2 <- lapply(dtable, gsub, pattern = "      ", replacement = " ")
-dtable2 <- lapply(dtable2, gsub, pattern = "     ", replacement = " ")
-dtable2 <- lapply(dtable2, gsub, pattern = "    ", replacement = " ")
-dtable2 <- lapply(dtable2, gsub, pattern = "   ", replacement = " ")
-dtable2 <- lapply(dtable2, gsub, pattern = "  ", replacement = " ")
-
-dsplit <- lapply(dtable2, strsplit, " ")
-dvec <- unlist(dsplit)
-dmat <- matrix(dvec, ncol=13, byrow=TRUE)
-dframe <- as.data.frame(dmat)
-
-write.csv(dframe, "data/pdo.csv")
-
-## OR, as Jon found read.fwf:
-if (file.exists("data/pdo.txt")) { 
-  file <- read.fwf(file = "data/pdo.txt", skip = 30, n = 118, 
-                   widths = c(8, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7))
-} else {
+## download PDO url and create data frame structure with numerics
+if (!file.exists("data/pdo.txt")) { 
   download.file("http://research.jisao.washington.edu/pdo/PDO.latest", "data/pdo.txt")
-  file <- read.fwf(file = "data/pdo.txt", skip = 30, n = 118, 
+  }
+
+file <- read.fwf(file = "data/pdo.txt", skip = 30, n = 118, 
                    widths = c(8, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7))
-}
 
 colnames(file) <- unlist(file[1,])
-pdo <- file[-c(1, 2),]
+pdo <- file[-c(1, 2),] # all as factors and straight to numeric gives gibberish
+pdo <- lapply(pdo, as.character)
+pdo <- lapply(pdo, as.numeric) #though this makes NA out of the years that have asterisks.
+#   these asterisks actually mean "Derived from OI.v2 SST fields" (erm...)
+pdo <- do.call(cbind, pdo)
+pdo[pdo == 99.9] <- NA
+pdo <- as.data.frame(pdo)
+names(pdo) <- c("year", "jan", "feb", "mar", "apr","may", "jun", "jul", "aug", "sep", "oct", "nov", 
+                "dec") # unlist() earlier maintained spaces in the names...
+allna <- which(is.na(pdo$year))
+pdo$year[allna] <- c(seq(from = 2002, length.out = length(allna), by = 1))
 
-if (file.exists("data/soi.txt")) { 
-  all_data <- read.fwf(file = "data/soi.txt", skip = 3, widths = rep(6, 13))
-} else {
+## create yearly means since I believe we want annual reso
+pdo <- transform(pdo, mean = rowMeans(pdo[,-1], na.rm = TRUE))
+
+## create rds for later
+saveRDS(pdo, "data/pdo.rds")
+
+## download SOI url and create data frames with numeric entries
+## FIXME: do we want standardised or unstandardised data?
+if (!file.exists("data/soi.txt")) { 
   download.file("http://www.cpc.noaa.gov/data/indices/soi", "data/soi.txt")
-  all_data <- read.fwf(file = "data/soi.txt", skip = 3, widths = rep(6, 13))
-}
-table1 <- all_data[c(1:66),] # unstandardised
+  }
+
+all_data <- read.fwf(file = "data/soi.txt", skip = 3, widths = rep(6, 13))
+
+## create separate df for the unstandardised data
+table1 <- all_data[c(1:66),] 
 colnames(table1) <- unlist(table1[1,])
-soinonst <- table1[-c(1),]
+soinonst <- table1[-c(1),] # all as factors and straight to numeric gives gibberish
+soinonst <- lapply(soinonst, as.character)
+soinonst <- lapply(soinonst, as.numeric)
+soinonst <- do.call(cbind, soinonst)
+soinonst[soinonst == 99.9] <- NA
+soinonst <- as.data.frame(soinonst)
 
-table2 <- all_data[c(75:140),] # standardised
+## create rds for later
+saveRDS(soinonst, "data/soi_nonstand.rds")
+
+## create separate df for the standardised data
+table2 <- all_data[c(75:140),] 
 colnames(table2) <- unlist(table2[1,])
-soist <- table2[-c(1),]
-## FIXME: do we want standardised or unstandardised?
-## FIXME: check that we can read all data as numeric etc.
+soist <- table2[-c(1),] # all as factors and straight to numeric gives gibberish
+soist <- lapply(soist, as.character)
+soist <- lapply(soist, as.numeric)
+soist <- do.call(cbind, soist)
+soist[soist == 99.9] <- NA
+soist <- as.data.frame(soist)
 
-## Part 2:
+## create rds for later
+saveRDS(soist, "data/soi_stand.rds")
+
+
+## download NAO url and make data frame with numeric entries
+if (!file.exists("data/nao.txt")) {
+  download.file("https://climatedataguide.ucar.edu/sites/default/files/nao_station_seasonal.txt",
+                "data/nao.txt")
+}
+
+naos <- readLines("data/nao.txt")
+naos <- naos[-c(1:2)]
+naos <- lapply(naos, gsub, pattern = "    ", replacement = " ")
+naos <- lapply(naos, gsub, pattern = "   ", replacement = " ")
+naos <- lapply(naos, gsub, pattern = "  ", replacement = " ")
+
+naosplit <- lapply(naos, strsplit, " ")
+naovec <- unlist(naosplit)
+naomat <- matrix(naovec, ncol=13, byrow=TRUE)
+naoframe <- as.data.frame(naomat)
+colnames(naoframe) <- c("year", "djf", "jfm", "fma", "mam", "amj", "mjj", "jja", "jas", "aso", 
+                      "son", "ond", "ndj")
+naoframe <- lapply(naoframe, as.character)
+naoframe <- lapply(naoframe, as.numeric)
+naoframe <- do.call(cbind, naoframe)
+naoframe <- as.data.frame(naoframe)
+naoframe[naoframe <= -999] <- NA
+
+## create rds for later
+saveRDS(naoframe, "naoseasonal.rds")
+
+## Part 2: 939 rows, 375 rows of one or more NAs with variable selection FIXMEs 
 ## ==================================================================================================
 
 ## read in supporting monitoring data tables grabbed from database
 ## I created a Date2 column in OpenOffice to convert the current display of month as character to month
-##    as numeric (former is how it came from database into excel cause excel sucks)
+##    as numeric (former is how it came from database into excel)
 
 routines <- read.csv("data/private/qpco2supportdata.csv")
 routines <- transform(routines, Date = as.POSIXct(as.character(Date2), format = "%Y-%m-%d"))
@@ -71,6 +112,8 @@ produc <- transform(produc, Date = as.POSIXct(as.character(Date2), format = "%Y-
 
 chl <- read.csv("data/private/Chl.csv")
 chl <- transform(chl, Date = as.POSIXct(as.character(Date2), format = "%Y-%m-%d"))
+
+gasflux <- readRDS("data/private/gasFlux.rds") # from pressuremanipulations.R
 
 ## remove extra date column (originally retained in case wanna check that the date format
 ##    conversion worked in OpenOffice)
@@ -96,8 +139,12 @@ mycolMeans <- function(df, cols) {
 }
 
 ## choose columns we want means for
-## FIXME: confirm with kerri that we want netoxy, and which respiration measure we want
-## FIXME: chl columns: if we take "total chl", there are NAs where method only measured chla ...
+## FIXME: confirm with kerri that we want netoxy, and which respiration measure we want; NP vs GPP?
+##    apparently this may be a macro that alain calculated in systat then R (see code that Nicole 
+##    commented and sent: AlainsCode.R in private/)
+## FIXME: is the variable O2 from the water column or the netoxy thing???
+## FIXME: chl columns: if we take "chl a" rather than total chl there are NAs --> implies different
+##    method used for total chl and individual chlorophylls...!?
 ##    so need to know if we are taking total or chl a ... and if we can replace missing data
 
 prodmeans <- lapply(prodsplit, mycolMeans, cols = c("NetOxy_ppm", "Resp_mgC_m3_H")) 
@@ -105,7 +152,7 @@ prodmeans <- do.call(rbind, prodmeans)
 rownames(prodmeans) <- NULL
 prodmeans <- prodmeans[with(prodmeans, order(LAKE, Date)),]
 
-chlmeans <- lapply(chlsplit, mycolMeans, cols = c("CHLC", "Total_chl")) 
+chlmeans <- lapply(chlsplit, mycolMeans, cols = c("Chl_a_ug_L", "Total_chl")) 
 chlmeans <- do.call(rbind, chlmeans)
 rownames(chlmeans) <- NULL
 chlmeans <- chlmeans[with(chlmeans, order(LAKE, Date)),]
@@ -113,17 +160,46 @@ chlmeans <- chlmeans[with(chlmeans, order(LAKE, Date)),]
 ## merge all dfs by LAKE and Date
 co2explained <- merge(chlmeans, prodmeans) ## FIXME: NAs are now NaN.. problem for later?
 co2explained <- merge(co2explained, routines)
+names(gasflux)[which(names(gasflux) == 'Lake')] <- "LAKE"
+co2expl <- merge(co2explained, gasflux[,c('Date', 'LAKE', 'Temperature')], by = c('Date', 'LAKE'))
+# this creates shorter data frame cause co2explained had R, E, M, WC in it and gasflux not. though 
+#   FIXME: diff of 100 rows indicated by nrow() of the dfs vs 97 rows in co2explained when subsetting
+#   nrow(subset(co2explained, LAKE %in% c("R", "E", "WC", "M")))
 
 ## save output for later
-saveRDS(co2explained, "data/private/co2explained.rds")
+saveRDS(co2expl, "data/private/co2explained.rds")
+
+## bonus: let's see how many NAs in our data. though see FIXME about variable selection
+dataloss <- subset(co2expl, select = c("LAKE", "Date", "pH_surface", "TIC_mg_L", "Temperature", 
+                                       "SRP_ug_L", "TDN_ug_L", "NO3_ug_L", "NH4_ug_L", "DOC_mg_L", 
+                                       "Chl_a_ug_L", "NetOxy_ppm", "Resp_mgC_m3_H"))
+
+nanumbers <- rowSums(is.na(dataloss))
+dataloss <- dataloss[rowSums(is.na(dataloss)) > 0,]
+ 
 
 ## Part 3:
 ## ==========================================================================================
 
 ## kerri emailed me this (in PhDPapers/Data..) pCO2_vars_yearlyaverageswithclimate.csv, but
 ##    it has only a few years, so we need to supplement it.
-## flow data can be sourced from https://www.wsask.ca/Lakes-and-Rivers/
+## flow data summaries can be sourced from https://www.wsask.ca/Lakes-and-Rivers/
 ##                               Stream-Flows-and-Lake-Levels/QuAppelle-River-Watershed/05JK007/
 ## but FIXME: need to know which site for which lake
 
-## ice-out, some flow data, from Rich (.../fromRich). but generally only to 2009-2011. very patchy data
+## ice-out, some flow data, from Rich (.../fromRich) and copied into git../private/. 
+##    but generally only to 2009-2011. very patchy data
+
+iceout1 <- read.csv("data/private/IceOut.csv") # pending data all the way to 2015
+icedataloss <- iceout1[rowSums(is.na(iceout1)) > 0,]
+nrow(iceout1)
+nrow(icedataloss)
+
+inflow1 <- read.csv("data/private/Monthly_Inflow_Estimates2.csv") # csv not currently in good format
+#   but just wanted to check how complete - data are complete.
+
+othervars <- read.csv("data/private/pCO2_vars_yearlyaverageswithclimate.csv")
+## this doesn't however indicate which vars interpolated and which real, but has the separate inflow
+##    sites and inflow data
+
+
