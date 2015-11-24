c97f93a3e64902f90fdb874dc04da0afe5d6c854
ewiik <eme.wiik@gmail.com>
2015-11-24 14:36:59 -0600

    CO2 flux calculations for Heather's sites

--- scripts/co2fluxcalc_heathersites.R
+++ scripts/co2fluxcalc_heathersites.R
@@ -29,6 +29,7 @@ alldat <- transform(alldat, co2flux = gasExchangeFlex(temp = temperature, cond =
                                                       pco2atm = pco2atm))
 
 ## a few diagnostic plots...
+pdf("data/private/heathersites.pdf")
 with(alldat, plot(co2flux ~ salinity))
 with(alldat, plot(co2flux ~ conductivity))
 with(alldat, plot(co2flux ~ Altitude))
@@ -36,8 +37,12 @@ with(alldat, plot(co2flux ~ wind))
 with(alldat, plot(co2flux ~ temperature))
 with(alldat, plot(co2flux ~ DIC))
 with(alldat, plot(co2flux ~ pH))
-with(alldat, plot(latitude ~ longitude, type = "n"))
-with(alldat, points(latitude ~ longitude, cex = abs(co2flux)/10))
-with(alldat, points(latitude ~ longitude, cex = abs(salinity/5)))
+with(alldat, plot(latitude ~ longitude, type = "p", pch = 25, bg = "black", cex = 0.4))
+with(alldat, points(latitude ~ longitude, cex = abs(co2flux)/10, 
+                    col = ifelse(co2flux < 0, "black", "purple")))
+with(alldat, points(latitude ~ longitude, cex = abs(salinity/5), col = "green"))
+legend("bottomright", cex = 0.8, legend = c("lake location", "salinity", "CO2 influx"), 
+       pch = c(25,1,1), pt.bg = c("black", "white", "white"), col = c("black", "green", "black"))
 # could also do as per symbols(x=dfx$ev1, y=dfx$ev2, circles=dfx$ev3, inches=1/3,
 # ann=F, bg="steelblue2", fg=NULL)
+dev.off()
