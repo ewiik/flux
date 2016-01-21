3a125bcaec65e2fe38dcd7d236bdfa3b7c175752
ewiik <eme.wiik@gmail.com>
2016-01-20 21:35:16 -0600

    some long term data checks on Helen's BP data

---
+++ scripts/buffalopound-baulch.R
@@ -0,0 +1,16 @@
+## quick look at Buffalo Pound monitoring data from 1985
+## base file based on xlsx in fromkerri/
+
+buff <- read.csv("data/private/BuffaloP_Data1985_2011.csv", na.strings = c("<NA>", "", "<"), 
+                 as.is = "ODOsat")
+buff$ODOsat
+buffs <- buff
+toreplace <- which(buff$ODOsat == NA | buff$ODOsat == "ERR")
+buff$ODOsat[toreplace] <- "NA"
+buff$ODOsat <- as.numeric(buff$ODOsat)
+buff <- buff[-which(buff$ODOsat > 200),]
+
+with(buff, plot(pH ~ ODOsat, col = week))
+
+bpsal <- subset(params, Lake == "B")
+     
