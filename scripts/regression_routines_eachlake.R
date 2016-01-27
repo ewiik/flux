5c7f04c441e99e89f9f863f222946bcca0a23058
ewiik <eme.wiik@gmail.com>
2016-01-26 20:04:00 -0600

    first script for looking into whether or not some lakes depart from global patterns

--- scripts/regression_routines_eachlake.R
+++ scripts/regression_routines_eachlake.R
@@ -9,9 +9,99 @@ if (!file.exists("../data/private/regvars.rds")) {
   source("../scripts/regression_routines.R")
 }
 regvars <- readRDS("../data/private/regvars.rds")
+regvarf <- regvars
+regvarf <- transform(regvarf, Year = as.factor(Year)) # make Year into factor for re
 
-## use final models created for CO2, though lapply this by lake (and therefore
-##    remove Lake as a random error)
+## split regvarf by lake then remove all NAs from list
+regsplit <- with(regvarf, split(regvarf, list(Lake)))
+regsplit <- regsplit[lapply(regsplit,nrow) > 1]
+
+## use final models created for CO2, adapted to see if any one Lake deviates
+##    from the global trend
+## =================================================================================
+## 1. model most similar to ones done before
+egmod <- gam(pH_surface ~ 
+               s(Chl_a_ug_L) + s(Chl_a_ug_L, by = Lake, m = 1) +
+               # m = 1 means that penalty goes with 1st derivative... required
+               #    here because we're doing both s() and s(..,by = ...)
+               s(GPP_h) + s(GPP_h, by = Lake, m = 1) +
+               s(TDN_ug_L) + s(TDN_ug_L, by = Lake, m = 1) + 
+               s(DOC_mg_L) + s(DOC_mg_L, by = Lake, m = 1) +
+               s(Oxygen_ppm) + s(Oxygen_ppm, by = Lake, m = 1) +
+               te(PDO, SOI) + te(PDO, SOI, by = Lake, m = 1) +
+               s(Lake, Year, bs = "re"), # still need to explicitly keep Lake as re!!!
+             data = regvarf,
+             select = FALSE, method = "REML", family = gaussian,
+             na.action = na.exclude,
+             control = gam.control(nthreads = 3, trace = TRUE))
+
+summary <- FALSE
+if(summary) {
+  summary(egmod)
+}
+
+## 2. In fact, good to log some of the more skewed distributions
+## change -ve values to 0, then add 1
+regvarf2 <- regvarf
+regvarf2$`Chl_a_ug_L`[regvarf2$`Chl_a_ug_L` <=0 ] <- 0
+regvarf2$`DOC_mg_L`[regvarf2$`DOC_mg_L` <=0 ] <- 0
+regvarf2$`Chl_a_ug_L` <- regvarf2$`Chl_a_ug_L` + 1
+regvarf2$`DOC_mg_L` <- regvarf2$`DOC_mg_L` + 1
+
+## for this model, we are removing some of the Lake-specificity where nothing signif
+##    was found in previous model
+egmod.red <- gam(pH_surface ~ 
+                   s(log10(Chl_a_ug_L)) + s(log10(Chl_a_ug_L), by = Lake, m = 1) +
+                   s(GPP_h) + s(GPP_h, by = Lake, m = 1) +
+                   s(log10(TDN_ug_L)) + 
+                   s(log10(DOC_mg_L)) +
+                   s(Oxygen_ppm) + s(Oxygen_ppm, by = Lake, m = 1) +
+                   te(PDO, SOI) +
+                   s(Lake, Year, bs = "re"),
+                 data = regvarf2,
+                 select = TRUE, method = "REML", family = gaussian,
+                 na.action = na.exclude,
+                 control = gam.control(nthreads = 3, trace = TRUE))
+                 # this setting makes the computation a bit more efficient 
+
+summary <- FALSE
+if(summary) {
+  summary(egmod.red)
+}
+
+## 3. model that removes further unnecessary elements to make fitting faster and simpler
+##    AND introduces scat() again. Testing to see if the step failure can be averted
+##    by setting maxHalf = 60
+egmod.red2 <- gam(pH_surface ~ 
+                    s(log10(Chl_a_ug_L)) + s(log10(Chl_a_ug_L), by = Lake, m = 1) +
+                    s(GPP_h) +
+                    s(log10(TDN_ug_L)) + 
+                    s(log10(DOC_mg_L)) +
+                    s(Oxygen_ppm) +
+                    te(PDO, SOI) +
+                    s(Lake, Year, bs = "re"),
+                  data = regvarf2,
+                  select = TRUE, method = "REML", family = scat(),
+                  na.action = na.exclude,
+                  control = gam.control(nthreads = 3, trace = TRUE,
+                                        newton = list(maxHalf = 60)))
+summary <- TRUE
+if(summary) {
+  summary(egmod.red2)
+}
+
+## plot output of model
+pdf("../docs/egmod-reduced-log-covariates.pdf")
+op <- par(mar = c(4,4,1,1) + 0.1)
+plot(egmod.red2, pages = 4, scheme = 2)
+par(op)
+dev.off()
+
+## ===========================================================================
+## Below are my initial apply applications before learning that I can introduce
+##    by = ... into a gam call which makes it all better cause I am still using 
+##    all the data that way and other obvious reasons
+## ===========================================================================
 co2gams <- function(df) {
   if (nrow(df) < 3) { return(NA) } else {
     co2gam <- gam(co2Flux ~ s(pH_surface, k = 20), data = df,
@@ -21,17 +111,13 @@ co2gams <- function(df) {
 
 resgams <- function(df, res) {
   if (nrow(df) < 3) { return(NA) } else {
-  res <- as.data.frame(res)
+  res <- as.vector(res)
     resmod <- gam(res ~ s(Chl_a_ug_L) + s(GPP_h) + s(TDN_ug_L) + 
              s(DOC_mg_L) + s(Oxygen_ppm) + 
              te(PDO, SOI) + s(Year, bs = "re"), data = df,
            select = TRUE, method = "REML", family = scat(),
            na.action = na.exclude) }
   }
-
-## split regvars by lake then remove all NAs from list
-regsplit <- with(regvars, split(regvars, list(Lake)))
-regsplit <- regsplit[lapply(regsplit,nrow) > 1]
 
 ## apply gam 
 regmods <- lapply(regsplit, co2gams)
@@ -40,12 +126,8 @@ regmods <- lapply(regsplit, co2gams)
 allres <- lapply(regmods, resid, type = "pearson", na.action = na.exclude)
 
 ## apply resgams to the residuals
+## FIXME: this not doing the right thing!
 resmods <- mapply(resgams, regsplit, allres)
-## FIXME: due to this error objects aren't created:
-##    Error in diag(c(rep(S11, rank), rep(0, p - rank))) : vector is too large 
-## with later model where res is made into data frame, I get this:
-##    Error in model.frame.default(formula = res ~ 1 + Chl_a_ug_L + GPP_h +  : 
-##    invalid type (list) for variable 'res' 
 
 ## can lapply these to the above to check diagnostics however
 ##    over gam.check the plotting default means I don't know how to store objects
@@ -55,3 +137,20 @@ regsumms <- lapply(regmods, summary)
 lapply(regsumms, '[[', "dev.expl")
 
 lapply(regmods, plot, pers = TRUE, pages = 1)
+
+
+## 2. pH ~ .
+## ===========================================================================
+phgams <- function(df) {
+  if (nrow(df) < 3) { return(NA) } else {
+  gam(pH_surface ~ s(Lake, Year, bs = "re") + s(Chl_a_ug_L) + s(GPP_h) +
+               s(TDN_ug_L) + s(DOC_mg_L) + s(Oxygen_ppm) + te(PDO, SOI), data = df,
+             select = TRUE, method = "REML", family = gaussian, na.action = na.exclude)
+}}
+
+phmods <- lapply(regsplit, phgams)
+
+summary(phmod)
+plot(phmod, pages = 1, pers = TRUE)
+gam.check(phmod)
+
