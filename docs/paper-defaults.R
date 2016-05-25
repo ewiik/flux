c962de416f3377ece5fc260473524332b039ad33
ewiik <eme.wiik@gmail.com>
2016-05-25 14:47:35 +0100

    updates for ASLO

--- docs/paper-defaults.R
+++ docs/paper-defaults.R
@@ -18,6 +18,21 @@ expression(paste(italic('p')*'CO'[2]))
 ylab(expression(paste(mu*'atm')))
 ~h^{-1}
 expression(paste("VAR ("~mu*"g"~L^{-1}*")"))
+expression(paste(CO[2]~"flux (mmol"~"C "*m^{-2}*"d"^{-1}*')'))
+
+## ggplot labels for strips
+varnames <- list(
+  'Temperature'=expression(paste("Temperature ("~degree*"C)")) ,
+  'Conductivity'= expression(paste("Conductivity ("*mu*"S"~"cm"^{-1}*")s")),
+  'pH'="pH",
+  'meanWindMS'= expression(paste("Mean Wind (m"~"s"^{-1}*")")),
+  'SalCalc' = "Salinity (ppt)",
+  'TICumol' = expression(paste("DIC ("~mu*"mol"~"L"^{-1}*")")),
+  'Pressure' = 'Pressure (kPa)',
+  'pco2atm' = expression(paste(italic(p)*"CO"[2]~"(ppm)"))
+)
+
+## how to get independent labeling to ggplot
 
 
   
