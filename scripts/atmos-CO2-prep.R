da3c69d883d2cefd55c6969e6b544eca4e1afde5
Emma <ew@localhost.localdomain>
2016-10-14 21:55:18 +0100

    improved script, now also with temperature, and proper ggplots

--- scripts/atmos-CO2-prep.R
+++ scripts/atmos-CO2-prep.R
@@ -12,15 +12,24 @@
 library("ggplot2")
 library("StreamMetabolism")
 library("mgcv")
+library("viridis")
 
 ## create a theme to save linespace in plots
 papertheme <- theme_bw(base_size=18, base_family = 'Arial') +
   theme(legend.position='top')
 
 ## read in data
-esther <- read.delim("~/not-to-backup/Esther-CO2-Hourly.DAT", sep = ",")
-bratt <- read.delim("~/not-to-backup/Bratts_Lake-CO2-Hourly.DAT", sep = ",")
-trout <- read.delim("~/not-to-backup/East_Trout_Lake-CO2-Hourly.DAT", sep = ",")
+if (!file.exists("../data/privateEsther-CO2-Hourly.DAT")) {
+  stop("get CO2 data files from Emma or EC")}
+esther <- read.delim("../data/private/Esther-CO2-Hourly.DAT", sep = ",")
+bratt <- read.delim("../data/private/Bratts_Lake-CO2-Hourly.DAT", sep = ",")
+trout <- read.delim("../data/private/East_Trout_Lake-CO2-Hourly.DAT", sep = ",")
+
+if (!file.exists("../data/dmet.rds")) {
+source("../scripts/gasexchange_pressuredata.R")}
+temp <- readRDS("../data/dmet.rds")
+reg <- subset(temp, StationID==3002 | StationID==51441) # subsetted with Bratt in mind
+reg$Time <- as.integer(reg$Time)
 
 ## put them all together
 bratt$Site <- "Bratt"
@@ -47,10 +56,13 @@ sundat <- transform(sundat, Year = as.numeric(format(sunrise, format = "%Y")))
 sundat <- transform(sundat, UpTime = as.numeric(sundat$sunrise - trunc(sundat$sunrise, "days")))
 sundat <- transform(sundat, DownTime = as.numeric(sundat$sunset - trunc(sundat$sunset, "days")))
 
-bet <- merge(bet, sundat) 
 bet$TOD <- ifelse(bet$HourEnding-0.5 < bet$DownTime & 
                     bet$HourEnding-0.5 > bet$UpTime, 
                   "Day", "Night")
+
+bet <- merge(bet, reg, by.x=c("Year", "Month", "Day", "HourEnding"), 
+             by.y=c("Year", "Month", "Day","Time"))
+names(bet)[grep("degC", names(bet))] <- c("temp", "dewtemp")
 
 ## plot them and look
 brattplot <- ggplot(data=bet[bet$Year>=2009 & bet$Site == "Bratt",], aes(y=Mean, x=DayofYear, col=TOD)) +
@@ -105,8 +117,85 @@ brattmodnull <- gam(Mean ~ ti(DayofYear, bs="cc", k=80) + s(Year, k=5) + ti(Hour
                   ti(DayofYear, HourEnding, bs = c("cc","cc")), 
                 data=bet[bet$Site == "Bratt",], family=scat(),
                 method="REML", select=TRUE)
-brattmod <- gam(Mean ~ s(Year, k=5) + te(DayofYear, HourEnding, bs = c("cc","cc"), k=c(20,15)), 
+brattmod <- gam(Mean ~ s(Year, k=5) + te(DayofYear, HourEnding, bs = c("cc","cc"), k=c(40,15)), 
                 data=bet[bet$Site == "Bratt",], family=scat(),
                 method="REML", select=TRUE)
+
+tempmod <- gam(temp ~ s(Year, k=5) + te(DayofYear, HourEnding, bs = c("cc","cc"), k=c(40,15)), 
+                data=bet[bet$Site == "Bratt",], family=gaussian,
+                method="REML", select=TRUE)
+
+## get expand grid object to plot a ggplot contour/heatmap
+preddf <- with(bet, expand.grid(HourEnding = seq(min(HourEnding), max(HourEnding), length = 300), 
+                                     DayofYear = seq(min(DayofYear), max(DayofYear), length = 300)))
+superN <- nrow(preddf)
+YearMean <- with(bet, mean(Year))
+preddf$Year <- YearMean
+
+comb.pred <- predict(brattmod, newdata = preddf, type = "terms")
+
+whichCols <- grep("Hour", colnames(comb.pred))
+comb.pdat <- cbind(preddf, Fitted = comb.pred[, whichCols])
+
+shiftcomb <- attr(comb.pred, "constant")
+comb.pdatnorm <- comb.pdat
+comb.pdatnorm <- with(comb.pdatnorm, transform(comb.pdatnorm, Fitted = Fitted + shiftcomb))
+
+#exclude things too far from real
+toofar <- exclude.too.far(comb.pdatnorm$DayofYear, comb.pdatnorm$HourEnding, 
+                          bet$DayofYear, bet$HourEnding, dist=0.1)
+comb.pdatnorm$CO2 <- comb.pdatnorm$Fitted
+comb.pdatnorm$CO2[toofar] <- NA
+
+names(comb.pdat)[which(names(comb.pdat)=='Fitted')] <- 'CO2'
+
+comboplot <- ggplot(comb.pdatnorm, aes(x = DayofYear, y =HourEnding, z=CO2)) + #, z=Fitted
+  theme_bw(base_size=14, base_family = 'Arial') +
+  theme(legend.position='top') +
+  geom_raster(aes(fill=CO2)) + # change to turn grey background into nothing
+  scale_fill_viridis(na.value='transparent', name=expression(paste(italic('p')*'CO'[2]*" (ppm)"))) +
+  #geom_point(data=bet, aes(x=DayofYear, y=HourEnding, z=NULL)) +
+  theme(legend.key.width=unit(2,"cm")) +
+  ylab("Hour") + xlab("Day of Year") +
+  guides(guide=guide_legend(title.vjust = 0.1))
+## FIXME: this does nothing to change the justification of pCO2!!
+
+## for temp:
+preddf <- with(bet, expand.grid(HourEnding = seq(min(HourEnding), max(HourEnding), length = 300), 
+                                DayofYear = seq(min(DayofYear), max(DayofYear), length = 300)))
+superN <- nrow(preddf)
+YearMean <- with(bet, mean(Year))
+preddf$Year <- YearMean
+
+comb.pred <- predict(tempmod, newdata = preddf, type = "terms")
+
+whichCols <- grep("Hour", colnames(comb.pred))
+comb.pdat <- cbind(preddf, Fitted = comb.pred[, whichCols])
+
+shiftcomb <- attr(comb.pred, "constant")
+comb.pdatnorm <- comb.pdat
+comb.pdatnorm <- with(comb.pdatnorm, transform(comb.pdatnorm, Fitted = Fitted + shiftcomb))
+
+#exclude things too far from real
+toofar <- exclude.too.far(comb.pdatnorm$DayofYear, comb.pdatnorm$HourEnding, 
+                          bet$DayofYear, bet$HourEnding, dist=0.1)
+comb.pdatnorm$temp <- comb.pdatnorm$Fitted
+comb.pdatnorm$temp[toofar] <- NA
+
+
+tempplot <- ggplot(comb.pdatnorm, aes(x = DayofYear, y =HourEnding, z=temp)) + #, z=Fitted
+  theme_bw(base_size=14, base_family = 'Arial') +
+  theme(legend.position='top') +
+  geom_raster(aes(fill=temp)) + # change to turn grey background into nothing
+  scale_fill_viridis(na.value='transparent', name=expression(paste("temperature"*'CO'[2]*" (ppm)"))) +
+  #geom_point(data=bet, aes(x=DayofYear, y=HourEnding, z=NULL)) +
+  theme(legend.key.width=unit(2,"cm")) +
+  ylab("Hour") + xlab("Day of Year") +
+  guides(guide=guide_legend(title.vjust = 0.1))
+## FIXME -- model seems to need an interaction for year and day of year ... 
+
+
 ## save plots and models
+saveRDS(brattmod, "../data/private/bratt-gam-co2time.rds")
+saveRDS(tempmod, "../data/private/bratt-gam-temptime.rds")
 ggsave("../docs/private/bratt-co2station.pdf", brattplot, scale=1.6)
