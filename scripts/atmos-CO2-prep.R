84ef7e5ef9855afb1f51a0c01019e77a2eeef5e2
Emma <ew@localhost.localdomain>
2016-10-17 19:00:09 +0100

    created by year plot for temperature and superposed CO2 contours on it all

--- scripts/atmos-CO2-prep.R
+++ scripts/atmos-CO2-prep.R
@@ -13,13 +13,14 @@ library("ggplot2")
 library("StreamMetabolism")
 library("mgcv")
 library("viridis")
+library("reshape2")
 
 ## create a theme to save linespace in plots
 papertheme <- theme_bw(base_size=18, base_family = 'Arial') +
   theme(legend.position='top')
 
 ## read in data
-if (!file.exists("../data/privateEsther-CO2-Hourly.DAT")) {
+if (!file.exists("../data/private/Esther-CO2-Hourly.DAT")) {
   stop("get CO2 data files from Emma or EC")}
 esther <- read.delim("../data/private/Esther-CO2-Hourly.DAT", sep = ",")
 bratt <- read.delim("../data/private/Bratts_Lake-CO2-Hourly.DAT", sep = ",")
@@ -56,34 +57,35 @@ sundat <- transform(sundat, Year = as.numeric(format(sunrise, format = "%Y")))
 sundat <- transform(sundat, UpTime = as.numeric(sundat$sunrise - trunc(sundat$sunrise, "days")))
 sundat <- transform(sundat, DownTime = as.numeric(sundat$sunset - trunc(sundat$sunset, "days")))
 
-bet$TOD <- ifelse(bet$HourEnding-0.5 < bet$DownTime & 
-                    bet$HourEnding-0.5 > bet$UpTime, 
-                  "Day", "Night")
-
 bet <- merge(bet, reg, by.x=c("Year", "Month", "Day", "HourEnding"), 
              by.y=c("Year", "Month", "Day","Time"))
 names(bet)[grep("degC", names(bet))] <- c("temp", "dewtemp")
+
+bet <- merge(bet, sundat)
+bet$TOD <- ifelse(bet$HourEnding-0.5 < bet$DownTime & 
+                    bet$HourEnding-0.5 > bet$UpTime, 
+                  "Day", "Night")
 
 ## plot them and look
 brattplot <- ggplot(data=bet[bet$Year>=2009 & bet$Site == "Bratt",], aes(y=Mean, x=DayofYear, col=TOD)) +
   papertheme +
   geom_point(alpha=0.3) +
-  facet_wrap("Year", ncol=4) +
-  scale_color_viridis(name="Time of Day", discrete=TRUE, option = "magma", direction = -1,
+  facet_wrap("Year", ncol=3) +
+  scale_color_viridis(discrete=TRUE, option = "magma", direction = -1,
                       begin=0, end=0.7)  +
   #scale_color_viridis(name="Hour") + for when plotting by Hour
   theme(legend.key.width=unit(2,"cm")) +
-  guides(colour = guide_legend(override.aes = list(alpha = 1))) +
-  ylab("CO2 (ppm)") + xlab("Day of Year (200 mid-July)")
+  guides(colour = guide_legend(override.aes = list(alpha = 1)), legend.title=element_blank()) +
+  ylab(expression(paste(italic('p')*'CO'[2]))) + xlab("Day of Year (200 ca mid-July)")
 
 ggplot(data=bet[bet$Year>=2009 & bet$Site == "Esther",], aes(y=Mean, x=DayofYear, col=TOD)) +
   papertheme +
   geom_point(alpha=0.3) +
   facet_wrap("Year", ncol=4) +
-  scale_color_viridis(name="Time of Day", discrete=TRUE, option = "magma", direction = -1,
+  scale_color_viridis(discrete=TRUE, option = "magma", direction = -1,
                       begin=0, end=0.7)  +
   #scale_color_viridis(name="Hour") + for when plotting by Hour
-  theme(legend.key.width=unit(2,"cm")) +
+  theme(legend.key.width=unit(2,"cm"), legend.title=element_blank()) +
   guides(colour = guide_legend(override.aes = list(alpha = 1))) +
   ylab("CO2 (ppm)") + xlab("Day of Year (200 mid-July)")
 
@@ -91,10 +93,10 @@ ggplot(data=bet, aes(y=Mean, x=DayofYear, col=TOD)) +
   papertheme +
   geom_point(alpha=0.3) +
   facet_grid(Year ~ Site) +
-  scale_color_viridis(name="Time of Day", discrete=TRUE, option = "magma", direction = -1,
+  scale_color_viridis(discrete=TRUE, option = "magma", direction = -1,
                       begin=0, end=0.7)  +
   #scale_color_viridis(name="Hour") + for when plotting by Hour
-  theme(legend.key.width=unit(2,"cm")) +
+  theme(legend.key.width=unit(2,"cm"), legend.title=element_blank()) +
   guides(colour = guide_legend(override.aes = list(alpha = 1))) +
   ylab("CO2 (ppm)") + xlab("Day of Year (200 mid-July)")
 
@@ -124,6 +126,10 @@ brattmod <- gam(Mean ~ s(Year, k=5) + te(DayofYear, HourEnding, bs = c("cc","cc"
 tempmod <- gam(temp ~ s(Year, k=5) + te(DayofYear, HourEnding, bs = c("cc","cc"), k=c(40,15)), 
                 data=bet[bet$Site == "Bratt",], family=gaussian,
                 method="REML", select=TRUE)
+tempmodflex <- gam(temp ~ s(Year, k=5) + te(DayofYear, HourEnding, bs = c("cc","cc"),
+                                            by=factor(Year)), 
+               data=bet[bet$Site == "Bratt",], family=gaussian,
+               method="REML", select=TRUE)
 
 ## get expand grid object to plot a ggplot contour/heatmap
 preddf <- with(bet, expand.grid(HourEnding = seq(min(HourEnding), max(HourEnding), length = 300), 
@@ -147,7 +153,7 @@ toofar <- exclude.too.far(comb.pdatnorm$DayofYear, comb.pdatnorm$HourEnding,
 comb.pdatnorm$CO2 <- comb.pdatnorm$Fitted
 comb.pdatnorm$CO2[toofar] <- NA
 
-names(comb.pdat)[which(names(comb.pdat)=='Fitted')] <- 'CO2'
+co2predicted <- comb.pdatnorm
 
 comboplot <- ggplot(comb.pdatnorm, aes(x = DayofYear, y =HourEnding, z=CO2)) + #, z=Fitted
   theme_bw(base_size=14, base_family = 'Arial') +
@@ -192,10 +198,50 @@ tempplot <- ggplot(comb.pdatnorm, aes(x = DayofYear, y =HourEnding, z=temp)) + #
   theme(legend.key.width=unit(2,"cm")) +
   ylab("Hour") + xlab("Day of Year") +
   guides(guide=guide_legend(title.vjust = 0.1))
-## FIXME -- model seems to need an interaction for year and day of year ... 
+## model seems to need an interaction for year and day of year ... 
 
+## for temp by year:
+preddf <- with(bet, expand.grid(HourEnding = seq(min(HourEnding), max(HourEnding), length = 300), 
+                                DayofYear = seq(min(DayofYear), max(DayofYear), length = 300)))
+superN <- nrow(preddf)
+YearN <- length(unique(bet$Year))
+YearZ <- unique(bet$Year)
+combdf <- data.frame(HourEnding = rep(preddf$HourEnding, times=YearN),
+                     DayofYear = rep(preddf$DayofYear, times=YearN),
+                     Year = rep(YearZ, each=superN))
+
+comb.pred <- predict(tempmodflex, newdata = combdf, type = "terms")
+
+whichCols <- grep("Hour", colnames(comb.pred))
+comb.pdat <- cbind(preddf, Fitted = rowSums(comb.pred[, whichCols]))
+comb.pdat$Year <- rep(YearZ, each=superN)
+
+shiftcomb <- attr(comb.pred, "constant")
+comb.pdatnorm <- comb.pdat
+comb.pdatnorm$Fitted <- comb.pdatnorm$Fitted + shiftcomb
+
+#exclude things too far from real
+toofar <- exclude.too.far(comb.pdatnorm$DayofYear, comb.pdatnorm$HourEnding, 
+                          bet$DayofYear, bet$HourEnding, dist=0.1)
+comb.pdatnorm[toofar,] <- NA
+
+tempplotflex <- ggplot(comb.pdatnorm, aes(x = DayofYear, y =HourEnding, z=Fitted, group=Year)) + 
+  theme_bw(base_size=14, base_family = 'Arial') +
+  theme(legend.position='top') +
+  geom_raster(aes(fill=Fitted)) + # change to turn grey background into nothing
+  scale_fill_viridis(na.value='transparent', name=expression(paste("Fill: Temperature ("~degree*"C)"))) +
+  #geom_point(data=bet, aes(x=DayofYear, y=HourEnding, z=NULL)) +
+  theme(legend.key.width=unit(2,"cm")) +
+  facet_wrap("Year") +
+  ylab("Hour") + xlab("Day of Year") +
+  geom_contour(inherit.aes = FALSE, aes(x=DayofYear, y=HourEnding, z=CO2, colour=..level..))+
+  scale_color_viridis(name=expression(paste("Lines: Multiyear mean"~italic('p')*'CO'[2]*" (ppm)")))
+
+
+  grid.arrange(comboplot, tempplot)
 
 ## save plots and models
 saveRDS(brattmod, "../data/private/bratt-gam-co2time.rds")
 saveRDS(tempmod, "../data/private/bratt-gam-temptime.rds")
+saveRDS(tempmodflex, "../data/private/bratt-gam-temptime-byyear.rds")
 ggsave("../docs/private/bratt-co2station.pdf", brattplot, scale=1.6)
