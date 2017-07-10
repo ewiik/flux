be21474adc0223c99cd2dff4923dc425e320d8ac
Emma Wiik <e.wiik@bangor.ac.uk>
2017-07-10 22:21:39 +0100

    migrated co2 plot here to produce all paper plots in one script, also made plot better

--- scripts/eachlake-gamplots-lagged.R
+++ scripts/eachlake-gamplots-lagged.R
@@ -27,6 +27,8 @@ weathers <- readRDS('../data/weathers.rds')
 egmodlagged <- readRDS("../data/private/egmodlaggedsimp.rds")
 egmodtdn <- readRDS("../data/private/egmodlaggedtdn.rds")
 
+co2mod <- readRDS("../data/private/co2mod.rds")
+
 ## change to match what was used to create the models
 regvars <- merge(regvars, weathers)
 regvarf <- regvars
@@ -183,6 +185,77 @@ boxgrid <- plot_grid(meltplot, Yearplot, ncol = 1, nrow=2,
                      rel_heights = c(3,1.5), labels = c('a.','b.'))
 ggsave("../docs/private/summaryfig.pdf", boxgrid, width=20, height=40, units = "cm")
 
+## CO2-pH model
+## generate predicted for co2 and ph 
+## co2 ~ ph: full model
+N <- 200
+varWant <- c("GPP_h", "TDN_ug_L", "DOC_mg_L", "Oxygen_ppm", "PDO", "SOI")
+lakeXbar <- with(regvarf2, do.call("rbind",
+                                   lapply(split(regvarf2[, varWant], droplevels(Lake)), 
+                                          colMeans, na.rm = TRUE)))
+lakeXbar <- transform(lakeXbar, Lake = factor(rownames(lakeXbar)))
+
+co2.pdat <- with(droplevels(regvarf2),
+                 data.frame(`pH_surface` = rep(seq(min(`pH_surface`, na.rm = TRUE),
+                                                   max(`pH_surface`, na.rm = TRUE),
+                                                   length = N),
+                                               nlevels(Lake)),
+                            Lake = rep(levels(Lake), each = N),
+                            Year = rep(2004, prod(nlevels(Lake), N)),
+                            dummy = rep(0, prod(nlevels(Lake), N))))
+co2.pdat <- merge(co2.pdat, lakeXbar)
+co2.pred <- predict(co2mod, newdata = co2.pdat, type = "terms", se.fit = TRUE)
+
+whichCols <- grep("pH", colnames(co2.pred$fit))
+whichColsSE <- grep("pH", colnames(co2.pred$se.fit))
+co2.pdat <- cbind(co2.pdat, Fitted = rowSums(co2.pred$fit[, whichCols]), 
+                  se.Fitted = rowSums(co2.pred$se.fit[, whichColsSE]))
+limits <- aes(ymax = Fitted + se.Fitted, ymin= Fitted - se.Fitted)
+
+co2.pdat <- with(co2.pdat, transform(co2.pdat, Fittedplus = Fitted + se.Fitted))
+co2.pdat <- with(co2.pdat, transform(co2.pdat, Fittedminus = Fitted - se.Fitted))
+
+## make into original limits
+shiftco2 <- attr(predict(co2mod, newdata = co2.pdat, type = "iterms"), "constant")
+co2.pdatnorm <- co2.pdat
+co2.pdatnorm <- with(co2.pdatnorm, transform(co2.pdatnorm, Fitted = Fitted + shiftco2))
+co2.pdatnorm <- merge(co2.pdatnorm, minmaxes)
+overs <- with(co2.pdatnorm, which(pH_surface < minpH_surface | pH_surface > maxpH_surface))
+co2.pdatnorm <- co2.pdatnorm[-overs,]
+
+labdatco2 <- data.frame(x = 7.5, y = meanco2 - 18, label = "mean flux")
+
+## add quantiles
+phquants <- quantile(regvarf2$pH_surface, c(.05,.95), na.rm = TRUE)
+co2.pdatnorm$realname <- with(co2.pdatnorm,ifelse(Lake == "WW", "Wascana", 
+                                                  ifelse(Lake == "D", "Diefenbaker",
+                                                         ifelse(Lake == 'K', "Katepwa", 
+                                                                ifelse(Lake == 'P', "Pasqua", 
+                                                                       ifelse(Lake == 'B', 'Buffalo Pound', 
+                                                                              ifelse(Lake=='L','Last Mountain',
+                                                                                     'Crooked')))))))
+co2plot <- ggplot(co2.pdatnorm, aes(x = pH_surface, y = Fitted, 
+                                    colour = realname, lty=realname)) +
+  papertheme + 
+  annotate("rect", xmin=phquants[1], xmax=phquants[2], ymin=-Inf, ymax=Inf, alpha = 0.1, fill='gray60') +
+  geom_line() +
+  #geom_ribbon(aes(ymin = Fittedminus, ymax = Fittedplus), 
+  #           alpha = 0.25, fill='white') +  
+  scale_linetype_manual(name='Lake', values = c("solid", "dotdash","longdash", "solid", "longdash", 
+                                                "solid", "solid")) +
+  scale_colour_manual(name="Lake", values = c("#b2abd2", "#5e3c99","#b2abd2", "#5e3c99", "#e66101",
+                                              "#5e3c99", "#5e3c99"))+
+  #geom_text(data = labdatco2, aes(label = label, x = x, y = y, size = 5), 
+  #         show.legend = FALSE, inherit.aes = FALSE) +
+  geom_abline(slope = 0, intercept = meanco2, linetype="dotted") +
+  geom_vline(xintercept = meanpH, linetype = 'dotted') +
+  geom_rug3(aes(x=pH_surface, y=co2Flux), data = regvarf2, stat = "identity", position = "identity", 
+            sides = "bl", na.rm = FALSE, show.legend = NA, inherit.aes = FALSE, alpha=0.3) +
+  #theme(legend.position='top') +
+  guides(colour=guide_legend(nrow=3, ncol=3, byrow =TRUE,title.position = 'left',
+                             override.aes=list(size=0.7)),
+         linetype = guide_legend(nrow=3, ncol=3, byrow=TRUE, keywidth = unit(3,'line'))) +
+  xlab('pH') + ylab(expression(paste('CO'[2]*' (mmol m'^{-2}*d^{-1}*')')))
 
 ## for SPEI
 N <- 200
@@ -245,13 +318,13 @@ lakeXbar <- with(regvarf2, do.call("rbind",
 lakeXbar <- transform(lakeXbar, Lake = factor(rownames(lakeXbar)))
 
 TDN.pdat <- with(droplevels(regvarf2),
-                data.frame(`TDN_ug_L` = rep(seq(min(`TDN_ug_L`, na.rm = TRUE),
-                                               max(`TDN_ug_L`, na.rm = TRUE),
-                                              length = N),
-                                         nlevels(Lake)),
-                       Lake = rep(levels(Lake), each = N),
-                      Year = rep(2004, prod(nlevels(Lake), N)),
-                     dummy = rep(0, prod(nlevels(Lake), N))))
+                 data.frame(`TDN_ug_L` = rep(seq(min(`TDN_ug_L`, na.rm = TRUE),
+                                                 max(`TDN_ug_L`, na.rm = TRUE),
+                                                 length = N),
+                                             nlevels(Lake)),
+                            Lake = rep(levels(Lake), each = N),
+                            Year = rep(2004, prod(nlevels(Lake), N)),
+                            dummy = rep(0, prod(nlevels(Lake), N))))
 TDN.pdat <- merge(TDN.pdat, lakeXbar)
 TDN.pred <- predict(egmodlaggedtdn, newdata = TDN.pdat, type = "terms", se.fit = TRUE)
 whichCols <- grep("TDN", colnames(TDN.pred$fit))
@@ -267,8 +340,8 @@ TDN.pdat <- with(TDN.pdat, transform(TDN.pdat, Fittedminus = Fitted - se.Fitted)
 shiftTDN <- attr(predict(egmodlagged, newdata = TDN.pdat, type = "iterms"), "constant")
 TDN.pdatnorm <- TDN.pdat
 TDN.pdatnorm <- with(TDN.pdatnorm, transform(TDN.pdatnorm, Fitted = Fitted + shiftTDN, 
-                                            Fittedplus = Fittedplus + shiftTDN, 
-                                           Fittedminus = Fittedminus + shiftTDN))
+                                             Fittedplus = Fittedplus + shiftTDN, 
+                                             Fittedminus = Fittedminus + shiftTDN))
 TDN.pdatnorm <- merge(TDN.pdatnorm, minmaxes)
 overs <- with(TDN.pdatnorm, which(TDN_ug_L < minTDN_ug_L | TDN_ug_L > maxTDN_ug_L))
 TDN.pdatnorm <- TDN.pdatnorm[-overs,]
@@ -647,6 +720,7 @@ metabplots <- grid.arrange(chlaplot, Oxygenplot, DOCplot, ncol = 2,
 ggsave("../docs/private/ph-metab-lagged.pdf", metabplots, scale=1.1, width=10) #width=28, height=18, units = 'cm'
 ggsave("../docs/private/ph-clim-lagged.pdf", climgam, scale=0.9, width = 8, height=13)
 ggsave("../docs/private/ph-TDN-lagged.pdf", TDNplot, scale=0.9, width = 6, height=4)
+ggsave("../docs/private/gam-plots-co2plot.pdf", co2plot, scale=1.1, width = 7, height=5.5)
 
 ## time plots
 ## ==================================================================================
